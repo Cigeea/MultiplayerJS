82777bdce32db1f19861c5cdbb21638a78e4cf58
Cigeea <aeegic@live.fr>
2023-09-06 18:46:49 +0200

    Adding movement detection to the sprite animation

--- MJS/src/actors/Players/PlayerAnimation.ts
+++ MJS/src/actors/Players/PlayerAnimation.ts
@@ -12,5 +12,20 @@ export class PlayerAnimations {
 
         //Use correct directional frame
         actor.graphics.use(actor.skinAnims[actor.facing][WALK]);
+        // const walkingMsLeft = actor.walkingMsLeft ?? 0;
+        const walkingMsLeft = 0;
+        const anim: ex.Animation = actor.graphics.current[0].graphic as ex.Animation;
+        if (actor.vel.x !== 0 || actor.vel.y !== 0 || walkingMsLeft > 0) {
+            anim.play();
+            // actor.graphics.current[0].graphic.play();
+            return;
+        }
+
+
+        anim.pause();
+        anim.goToFrame(0);
+        // actor.graphics.current[0].graphic.pause();
+        // actor.graphics.current[0].graphic.goToFrame(0);
+
     }
 }
