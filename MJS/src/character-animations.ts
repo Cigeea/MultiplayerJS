56ef9f695d2800fa22a06b9bdb407aead0fa006b
Cigeea <aeegic@live.fr>
2023-09-13 23:09:08 +0200

    Tests on reduce syntax vs forEach syntax.
    Do not forget [] around property name in the reducer function

--- MJS/src/character-animations.ts
+++ MJS/src/character-animations.ts
@@ -62,21 +62,105 @@ export const generateCharacterAnimations = (spriteSheetKey: keyof typeof SPRITES
   // let payload: { [direction: string]: { [posture: string]: ex.Animation } } = {};
   // let payload: { [direction in DIRECTION]: { [posture in POSE]: ex.Animation } } = {};
   // let payload: Partial<Record<DIRECTION, Partial<Record<POSE, ex.Animation>>>> = {};
-  let payload: Partial<{ [direction in DIRECTION]: Partial<{ [posture in POSE]: ex.Animation }> }> = {};
-  [UP, DOWN, LEFT, RIGHT].forEach((dir) => {
-    payload[dir] = {};
-    [WALK, SWORD1, SWORD2, PAIN].forEach((pose) => {
-      const [frames, speed] = ANIMATION_CONFIGS[dir][pose];
-      payload[dir]![pose] = ex.Animation.fromSpriteSheet(
-        sheet,
-        [...frames],
-        speed
-      );
-    });
-  });
+  // let payload: Partial<{ [direction in DIRECTION]: Partial<{ [posture in POSE]: ex.Animation }> }> = {};
+  // [UP, DOWN, LEFT, RIGHT].forEach((dir) => {
+  //   payload[dir] = {};
+  //   [WALK, SWORD1, SWORD2, PAIN].forEach((pose) => {
+  //     const [frames, speed] = ANIMATION_CONFIGS[dir][pose];
+  //     payload[dir]![pose] = ex.Animation.fromSpriteSheet(
+  //       sheet,
+  //       [...frames],
+  //       speed
+  //     );
+  //   });
+  // });
 
-  return payload;
+  type Tata = Partial<{ [posture in POSE]: ex.Animation }>;
+  type Toto = Partial<{ [direction in DIRECTION]: Tata }>;
+  let payload2: Toto = {};
+
+  const arrDir: DIRECTION[] = [UP, DOWN, LEFT, RIGHT];
+  const arrPos: POSE[] = [PAIN, WALK, SWORD1, SWORD2];
+
+  payload2 = arrDir.reduce(callBack1, {});
+  function callBack1(acc1: Toto, currentVal1: DIRECTION) {
+    acc1 = {
+      ...acc1, [currentVal1]: arrPos.reduce((acc2: Tata, currentVal2: POSE) => {
+        const [frames, speed] = ANIMATION_CONFIGS[currentVal1][currentVal2];
+        return {
+          ...acc2, [currentVal2]: ex.Animation.fromSpriteSheet(
+            sheet,
+            [...frames],
+            speed
+          )
+        };
+      }, {})
+    }
+    return acc1;
+  }
+
+
+
+  // let payload2: Partial<{ [direction in DIRECTION]: Partial<{ [posture in POSE]: ex.Animation }> }> = {};
+  // let initialValue: Partial<{ [direction in DIRECTION]: Partial<{ [posture in POSE]: ex.Animation }> }> = {};
+
+  // payload2 = [UP, DOWN, LEFT, RIGHT].reduce(
+  //   (acc: Partial<{ [direction in DIRECTION]: Partial<{ [posture in POSE]: ex.Animation }> }>, direction) => {
+  //     acc[direction] = [WALK, SWORD1, SWORD2, PAIN].reduce(
+  //       (acc: Partial<{ [direction in DIRECTION]: Partial<{ [posture in POSE]: ex.Animation }> }>, pose) => {
+  //         const [frames, speed] = ANIMATION_CONFIGS[direction][pose];
+  //         // acc[direction][pose] = ex.Animation.fromSpriteSheet(
+  //         //   sheet,
+  //         //   [...frames],
+  //         //   speed);
+  //         acc[direction][pose] = 2;
+  //         return acc;
+  //       }, {}
+  //     );
+  //     return acc
+  //   }, initialValue);
+
+  return payload2;
 };
 
 
+
+
+
+// type LETTERS = 'A' | 'B' | 'C';
+// type ELEMENT = 'FIRE' | 'ICE' | 'THUNDER';
+// type COLORS = 'BLACK' | 'RED' | 'BROWN' | 'BLUE' | 'YELLOW';
+
+// function getColorsByLettersAndElement(letter: LETTERS, elem: ELEMENT): COLORS {
+//   console.log('GET : ' + letter + " " + elem);
+//   if (letter === 'A') {
+//     return "BLACK";
+//   }
+//   if (elem === 'ICE') {
+//     return "BLUE";
+//   }
+//   return 'RED';
+// }
+
+// const arrLet: LETTERS[] = ['A', 'B', 'C'];
+// const arrElem: ELEMENT[] = ['FIRE', 'ICE', 'THUNDER'];
+// let x = arrLet.reduce((accumLetters, currentLetter) => {
+//   console.log('Reducing for ' + currentLetter);
+//   let obj = arrElem.reduce((accumElem, currentElem) => {
+//     console.log('     Reducing for ' + currentElem);
+//     console.log('                  accumElem ->   '); console.dir(accumElem);
+//     return { ...accumElem, [currentElem]: getColorsByLettersAndElement(currentLetter, currentElem) };
+//   }, {})
+//   console.log('>>>>>>>>>>>>>>>>>> : '); console.dir(obj);
+//   return { ...accumLetters, [currentLetter]: obj };
+// }, {});
+
+// console.log(x);
+
+
+// let y = arrLet.reduce((acc,currentVal) => {
+//     return {...acc,[currentVal]: 'YEAH'};
+// },{})
+
+// console.log(y);
 
