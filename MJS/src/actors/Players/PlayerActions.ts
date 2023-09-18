24712c6c360e8a299b19b9e067a07dd238028094
Cigeea <aeegic@live.fr>
2023-09-18 18:54:55 +0200

    Adding types and dirty checks (? and !) to clean later

--- MJS/src/actors/Players/PlayerActions.ts
+++ MJS/src/actors/Players/PlayerActions.ts
@@ -2,6 +2,7 @@ import * as ex from 'excalibur';
 import { SpriteSequence } from '../../classes/SpriteSequence';
 import { SWORD1, SWORD2, SWORDACTION } from '../../constants';
 import { Player } from './Player';
+import { SWORD_SWING_1, SWORD_SWING_2, SWORD_SWING_3, Sword } from '../Sword';
 export class PlayerActions {
 
     actor: Player;
@@ -24,9 +25,9 @@ export class PlayerActions {
                 {
                     frame: actor.skinAnims[actor.facing]![SWORD1]!,
                     duration: SWORD_SWING_SPEED,
-                    actorObjCallback: (swordInstance) => {
+                    actorObjCallback: (swordInstance: Sword | null) => {
                         //change sword's frame to match character on frame 1
-                        //swordInstance.useFrame(SWORD_SWING_1, actor.facing);
+                        swordInstance?.useFrame(SWORD_SWING_1, actor.facing);
                     }
                 },
                 {
@@ -34,7 +35,7 @@ export class PlayerActions {
                     duration: SWORD_SWING_SPEED,
                     actorObjCallback: (swordInstance) => {
                         //change sword's frame to match character on frame 1
-                        //swordInstance.useFrame(SWORD_SWING_1, actor.facing);
+                        swordInstance?.useFrame(SWORD_SWING_2, actor.facing);
                     }
                 },
                 {
@@ -42,7 +43,7 @@ export class PlayerActions {
                     duration: SWORD_SWING_SPEED * 2,
                     actorObjCallback: (swordInstance) => {
                         //change sword's frame to match character on frame 1
-                        //swordInstance.useFrame(SWORD_SWING_1, actor.facing);
+                        swordInstance?.useFrame(SWORD_SWING_3, actor.facing);
                     }
                 }
             ],
@@ -50,5 +51,13 @@ export class PlayerActions {
                 actor.actionAnimation = null;
             }
         )
+
+        //Create the sword here...
+        const sword = new Sword(actor.pos.x, actor.pos.y, actor.facing);
+        engine?.add(sword);
+        sword.owner = actor;
+
+        // Assign this sword instance to be controllable by each frame above
+        actor.actionAnimation.actorObject = sword;
     }
 }
